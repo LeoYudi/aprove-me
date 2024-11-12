d3a530116aa350855a757b4d652727f10292ec84
Leonardo Higuti <lyhiguti@gmail.com>
2024-11-12 16:32:11 -0300

    refaactor: add proper body validation

--- backend/src/dtos/createAssignorBody.ts
+++ backend/src/dtos/createAssignorBody.ts
@@ -1,23 +1,38 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateAssignorBody {
   @IsNotEmpty({
-    message: 'O campo "document" não pode ser vazio',
+    message: 'O campo `document` não pode ser vazio',
+  })
+  @MaxLength(30, {
+    message: 'O campo `document` deve ser menor que 30 caracteres',
   })
   document: string;
 
   @IsNotEmpty({
-    message: 'O campo "email" não pode ser vazio',
+    message: 'O campo `email` não pode ser vazio',
+  })
+  @MaxLength(140, {
+    message: 'O campo `email` deve ser menor que 140 caracteres',
+  })
+  @IsEmail(undefined, {
+    message: 'O campo `email` é inválido',
   })
   email: string;
 
   @IsNotEmpty({
-    message: 'O campo "phone" não pode ser vazio',
+    message: 'O campo `phone` não pode ser vazio',
+  })
+  @MaxLength(20, {
+    message: 'O campo `phone` deve ser menor que 20 caracteres',
   })
   phone: string;
 
   @IsNotEmpty({
-    message: 'O campo "name" não pode ser vazio',
+    message: 'O campo `name` não pode ser vazio',
+  })
+  @MaxLength(140, {
+    message: 'O campo `name` deve ser menor que 140 caracteres',
   })
   name: string;
 }
